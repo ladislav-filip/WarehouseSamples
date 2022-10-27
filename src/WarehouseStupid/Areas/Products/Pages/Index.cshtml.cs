8c90ea597b482fe8601be7c71718ae67856a8f5a
Ladislav Filip <ladislav.filip@gmail.com>
2022-10-27 10:03:20 +0200

    Produkty

---
+++ src/WarehouseStupid/Areas/Products/Pages/Index.cshtml.cs
@@ -0,0 +1,26 @@
+﻿using Dapper;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Data.Sqlite;
+
+namespace WarehouseStupid.Areas.Products.Pages;
+
+public class ProductsIndexModel : PageModel
+{
+    private readonly ILogger<ProductsIndexModel> _logger;
+    private readonly SqliteConnection _connection;
+
+    public record ProductRec(long ProductId, string Code, string Name, long Weight);
+
+    public IEnumerable<ProductRec> Products;
+
+    public ProductsIndexModel(ILogger<ProductsIndexModel> logger, SqliteConnection connection)
+    {
+        _logger = logger;
+        _connection = connection;
+    }
+    
+    public void OnGet()
+    {
+        Products = _connection.Query<ProductRec>("SELECT * FROM products ORDER BY Name");
+    }
+}
